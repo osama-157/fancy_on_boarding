2b0aa3920a7eb3471fb93c3a59c6f52f50cfb513
osama-157 <57213006+osama-157@users.noreply.github.com>
2021-06-07 11:53:46 +0400

    c

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/xsahil03x/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/xsahil03x/AndroidStudioProjects/fancy_on_boarding/example"
+export "FLUTTER_ROOT=/Users/invospark/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/invospark/Documents/GitHub/fancy_on_boarding/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
